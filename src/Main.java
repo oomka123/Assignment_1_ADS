bba27cb37733ab4201a4eb87e3f9e4cafb445fe5
Kazbek <k.ahmetov123@gmail.com>
2025-03-25 09:34:06 +0600

    pushed

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(gcd(n, k));
+    }
+}
+
+
