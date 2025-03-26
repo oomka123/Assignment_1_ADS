6061249d36c2e99b45d339fa07751ce4d15cbaf5
Kazbek <k.ahmetov123@gmail.com>
2025-03-26 15:01:39 +0600

    pushed

--- src/Main.java
+++ src/Main.java
@@ -3,80 +3,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int minElementInArray(int[] nums, int length){
-
-        if(length == 1) return nums[0];
-
+    /**
+     * Finds the minimum element in an array recursively.
+     *
+     * @param nums   The array of integers.
+     * @param length The current length of the array being processed.
+     * @return The minimum element in the array.
+     *
+     * Time Complexity: O(n) - Each element is checked once.
+     */
+    public static int minElementInArray(int[] nums, int length) {
+        if (length == 1) return nums[0];
         return Math.min(nums[length - 1], minElementInArray(nums, length - 1));
-
-    }
-
-    public static float avarageInArray(int[] nums, int length){
-
-        if(length == 1) return nums[0];
-
-        return nums[length-1] + avarageInArray(nums, length - 1);
-
-    }
-
-    public static boolean isPrime(int num){
-
-        if(num % 2 == 0) return true;
-        return false;
-
-    }
-
-    public static int fact(int num){
-
-        if (num == 1) return 1;
-        return num * fact(num - 1);
-
-    }
-
-    public static int fibonacci(int num){
-        if(num == 0) return 0;
-        if (num == 1) return 1;
-        return fibonacci(num - 1) + fibonacci(num - 2);
-
-    }
-
-    public static int powerOf(int num, int power){
-        if (power == 0) return 1;
-        return num * powerOf(num, power - 1);
-
-    }
-
-    public static void reverseArray(int length, Scanner scanner) {
-        if (length == 1) {
-            System.out.print(scanner.nextInt() + " ");
-            return;
-        }
-        int num = scanner.nextInt();
-        reverseArray(length - 1, scanner);
-        System.out.print(num + " ");
-    }
-
-    public static boolean isAllDigit(char[] str, int index) {
-        if (index == str.length) return true;
-        if (!Character.isDigit(str[index])) return false;
-        return isAllDigit(str, index + 1);
-    }
-
-    public static int binomialCoefficient(int n, int k) {
-        if (k == 0 || k == n) return 1;
-        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
-    }
-
-    public static int gcd(int a, int b) {
-        if(b == 0) return a;
-        return gcd(b, a % b);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        System.out.println(gcd(n, k));
+
+        // Min element in array
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        long startTime = System.nanoTime();
+        int minResult = minElementInArray(arr, size);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000; // nanoseconds to milliseconds
+        System.out.println("Minimum element of the array: " + minResult);
+        System.out.println("Time taken: " + duration + " milliseconds");
     }
 }
 
