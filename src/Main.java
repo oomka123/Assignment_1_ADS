930ca4020e525a30cd9a672f07f25643a69a68c8
Kazbek <k.ahmetov123@gmail.com>
2025-03-26 15:43:32 +0600

    2 task pushed

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,20 @@ public class Main {
         return Math.min(nums[length - 1], minElementInArray(nums, length - 1));
     }
 
+    /**
+     * Calculates the sum of all elements in an array recursively.
+     *
+     * @param nums   The array of integers.
+     * @param length The current length of the array being processed.
+     * @return The sum of the array elements.
+     *
+     * Time Complexity: O(n) - Each element is added once.
+     */
+    public static float sumInArray(int[] nums, int length) {
+        if (length == 1) return nums[0];
+        return nums[length - 1] + sumInArray(nums, length - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -28,11 +42,27 @@ public class Main {
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
         int minResult = minElementInArray(arr, size);
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime) / 1000000; //  nanoseconds to milliseconds
+        long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
         System.out.println("Minimum element of the array: " + minResult);
+        System.out.println("Time taken: " + duration + " milliseconds");
+
+        // Sum of elements in array
+        int[] arr2 = new int[size];
+        System.out.println("Enter the size of the array: ");
+        int size2 = sc.nextInt();
+        System.out.println("Enter the array elements again: ");
+        for (int i = 0; i < size; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        startTime = System.currentTimeMillis();
+        float sumResult = sumInArray(arr2, size2);
+        endTime = System.currentTimeMillis();
+        duration = (endTime - startTime);
+        System.out.println("The sum of the array elements: " + sumResult);
         System.out.println("Time taken: " + duration + " milliseconds");
     }
 }
